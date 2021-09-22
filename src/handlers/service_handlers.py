62f120a31f138eab20a839f4ea9aa80ad9b49e40
Michael Savin <jtprogru@gmail.com>
2021-09-22 14:42:02 +0300

    Add admin and service handlers
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- src/handlers/service_handlers.py
+++ src/handlers/service_handlers.py
@@ -1,12 +1,12 @@
 import logging
-from telegram import ParseMode, Update
+from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def delete_voices(update: Update, context: CallbackContext) -> None:
+def delete_bad_message(update: Update, context: CallbackContext) -> None:
     """Delete all voice, video_note, location, left_chat_member, new_chat_members messages"""
 
     logger.debug(f"[DEBUG] Message from {update.message.from_user} with ID {update.message.from_user.id} was deleted")
@@ -16,5 +16,14 @@ def delete_voices(update: Update, context: CallbackContext) -> None:
 def ping(update: Update, context: CallbackContext) -> None:
     """Ping->Pong"""
 
-    logger.debug(f"[DEBUG] Ping message from {update.message.from_user} with " f"ID {update.message.from_user.id} was response")
+    logger.debug(f"[DEBUG] Ping message from {update.message.from_user} with ID {update.message.from_user.id} was response")
     update.effective_message.reply_text("<b>pong</b>", parse_mode=ParseMode.HTML)
+
+
+def errors_logging(update: Update, context: CallbackContext, error: TelegramError) -> None:
+    """Errors logging"""
+
+    if not (error.message == "Message is not modified"):
+        logger.warning('Update "%s" caused error "%s"' % (update, error))
+    logger.error(f"[ERROR] {context.error.with_traceback}")
+
